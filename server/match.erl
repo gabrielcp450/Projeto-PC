35fd86ac24a03384d3a7d52d5303efa1094e2aa8
gabrielcp450 <gspaiva2@gmail.com>
2025-05-06 15:19:14 +0100

    colission and keys functions

--- server/match.erl
+++ server/match.erl
@@ -1,7 +1,8 @@
 -module(match).
 -export([create/2]).
 
--record(player, {p = {0, 0}, v = {0.1, 0}, a = {0, 0}, points = 0, proj_v = 0, proj_i = 0 }).
+-record(keys, {w = false, s = false, a = false, d = false}).
+-record(player, {p = {0, 0}, v = {0.1, 0}, a = {0, 0}, points = 0, proj_v = 0, proj_i = 0, k = #keys{}}).
 -define(TICK, 500).
 
 initial_pos(I) ->
@@ -33,9 +34,65 @@ movement(Pids) ->
 create(Pid1, Pid2) -> 
     register(player1, Pid1),
     register(player2, Pid2),
-    Pids = #{Pid1 => initial_pos(0), Pid2 => initial_pos(1)},
+    Pids = #{player1 => initial_pos(0), player2 => initial_pos(1)},
     Pid = spawn(fun() -> loop(Pids) end),
     timer:send_after(5000, Pid, finished).
+
+pressed(Pid, Pids, K) ->
+    Player = maps:get(Pid, Pids),
+    Keys = Player#player.k,
+    NewKeys = case K of
+        w -> Keys#keys{w = true};
+        s -> Keys#keys{s = true};
+        a -> Keys#keys{a = true};
+        d -> Keys#keys{d = true}
+    end,
+    NewPlayer = Player#player{k = NewKeys},
+    NewPlayer.
+
+unpressed(Pid, Pids, K) ->
+    Player = maps:get(Pid, Pids),
+    Keys = Player#player.k,
+    NewKeys = case K of
+        w -> Keys#keys{w = false};
+        s -> Keys#keys{s = false};
+        a -> Keys#keys{a = false};
+        d -> Keys#keys{d = false}
+        end,
+    NewPlayer = Player#player{k = NewKeys},
+    NewPlayer.
+
+collision_walls(Pids) ->
+    Player1 = maps:get(player1, Pids),
+    Player2 = maps:get(player2, Pids),
+    {X1, Y1} = Player1#player.p,
+    {X2, Y2} = Player2#player.p,
+    if 
+        X1 =< 0 orelse X1 >= 1 orelse Y1 =< 0 orelse Y1 >= 1 ->
+            Points = Player2#player.points,
+            Player1New = Player1#player{p = (initial_pos(0))#player.p},
+            Player2New = Player2#player{
+                p = (initial_pos(1))#player.p,
+                points = Points + 2
+            },
+            UpdatedPids = Pids#{player1 => Player1New, player2 => Player2New},
+            [Pid ! {pos, Player#player.p} || {Pid, Player} <- maps:to_list(UpdatedPids)],
+            UpdatedPids;
+        X2 =< 0 orelse X2 >= 1 orelse Y2 =< 0 orelse Y2 >= 1 ->
+            Points = Player1#player.points,
+            Player2New = Player2#player{p = (initial_pos(1))#player.p},
+            Player1New = Player1#player{
+                p = (initial_pos(0))#player.p,
+                points = Points + 2
+            },
+            UpdatedPids = Pids#{player1 => Player1New, player2 => Player2New},
+            [Pid ! {pos, Player#player.p} || {Pid, Player} <- maps:to_list(UpdatedPids)],
+            UpdatedPids;
+        true ->
+            [Pid ! {pos, Player#player.p} || {Pid, Player} <- maps:to_list(Pids)],
+            Pids
+    end.
+
 
 loop(Pids) ->
     timer:send_after(?TICK, self(), update),
@@ -43,34 +100,16 @@ loop(Pids) ->
     receive 
         update -> 
             NewPids = movement(Pids),
-            Player1 = maps:get(player1, NewPids),
-            Player2 = maps:get(player2, NewPids),
-            {X1, Y1} = Player1#player.p,
-            {X2, Y2} = Player2#player.p,
-            if 
-                X1 =< 0 orelse X1 >= 1 orelse Y1 =< 0 orelse Y1 >= 1 ->
-                    Points = Player2#player.points,
-                    Player1New = Player1#player{p = (initial_pos(0))#player.p},
-                    Player2New = Player2#player{
-                        p = (initial_pos(1))#player.p,
-                        points = Points + 2
-                    },
-                    UpdatedPids = NewPids#{player1 => Player1New, player2 => Player2New},
-                    [Pid ! {pos, Player#player.p} || {Pid, Player} <- maps:to_list(UpdatedPids)],
-                    loop(UpdatedPids);
-                X2 =< 0 orelse X2 >= 1 orelse Y2 =< 0 orelse Y2 >= 1 ->
-                    Points = Player1#player.points,
-                    Player2New = Player1#player{p = (initial_pos(1))#player.p},
-                    Player1New = Player2#player{
-                        p = (initial_pos(0))#player.p,
-                        points = Points + 2
-                    },
-                    UpdatedPids = NewPids#{player1 => Player1New, player2 => Player2New},
-                    [Pid ! {pos, Player#player.p} || {Pid, Player} <- maps:to_list(UpdatedPids)],
-                    loop(UpdatedPids)
-                end,
-
-            [Pid ! {pos, Player#player.p} || {Pid, Player} <- maps:to_list(NewPids)],
+            NewPids2 = collision_walls(NewPids),
+            [Pid ! {pos, Player#player.p} || {Pid, Player} <- maps:to_list(NewPids2)],
+            loop(NewPids2);
+        {Pid, pressed, K} ->
+            NewPlayer = pressed(Pid, Pids, K),
+            NewPids = maps:put(Pid, NewPlayer, Pids),
+            loop(NewPids);
+        {Pid, unpressed, K} ->
+            NewPlayer = unpressed(Pid, Pids, K),
+            NewPids = maps:put(Pid, NewPlayer, Pids),
             loop(NewPids);
         finished ->
             [{Pid1, _}, {Pid2, _}] = maps:to_list(Pids),
@@ -86,5 +125,7 @@ loop(Pids) ->
                     {draw, draw}
             end,
             Pid1 ! {finished, Result1},
-            Pid2 ! {finished, Result2}
+            Pid2 ! {finished, Result2},
+            unregister(player1),
+            unregister(player2)
     end.
