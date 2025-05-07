5e8518333020d6012ec275f9c56cb0b09dccaf29
Edgar Araújo <edgararaj@gmail.com>
2025-05-07 18:47:57 +0100

    remove old javax client

--- client/crun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mvn clean package && java -cp target/duelo-client-1.0-SNAPSHOT.jar com.duelo.client.Main
-
--- client/run.sh
+++
@@ -1,1 +0,0 @@
-java -cp target/duelo-client-1.0-SNAPSHOT.jar com.duelo.client.Main
