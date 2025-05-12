65c3735e0d0bdc99ef38efc2c363e1ffd155f0f1
gabrielcp450 <gspaiva2@gmail.com>
2025-05-12 18:23:45 +0100

    move working

--- server/match.erl
+++ server/match.erl
@@ -2,12 +2,12 @@
 -export([create/2]).
 
 -record(keys, {w = false, s = false, a = false, d = false}).
--record(player, {p = {0, 0}, v = {0, 0}, a = {0, 0}, points = 0, proj_v = 0, proj_i = 0, k = #keys{}}).
--define(TICK, 500).
+-record(player, {p = {0, 0}, v = {0, 0}, a = {0, 0}, points = 0, proj_v = 0, proj_i = 0, k = #keys{}, first = 0}).
+-define(TICK, 9000).
 
 initial_pos(I) ->
     Px = 1/2 * (I+1) - 1/4,
-    #player{p = {Px, 1/2}}.
+    #player{p = {Px, 1/2}, first = I}.
 
 accelaration(Player) -> 
     {Ax, Ay} = Player#player.a,
@@ -19,17 +19,17 @@ accelaration(Player) ->
 
     NewAx = case {D, A} of 
         {true, false} ->
-            Ax + 0.1;
+            Ax + 0.001;
         {false, true} ->
-            Ax -0.1;
+            Ax -0.001;
         _ ->
             Ax
         end,
     NewAy = case {W, S} of
         {true, false} ->
-            Ay + 0.1;
+            Ay + 0.001;
         {false, true} ->
-            Ay -0.1;
+            Ay -0.001;
         _ ->
             Ay
         end,
@@ -59,9 +59,7 @@ movement(Pids) ->
     #{Pid1 => movement_player(Player1), Pid2 => movement_player(Player2)}.
 
 create(Pid1, Pid2) -> 
-    register(player1, Pid1),
-    register(player2, Pid2),
-    Pids = #{player1 => initial_pos(0), player2 => initial_pos(1)},
+    Pids = #{Pid1 => initial_pos(0), Pid2 => initial_pos(1)},
     MatchPid = spawn(fun() -> loop(Pids) end),
     timer:send_after(5000000, MatchPid, finished),
     MatchPid. 
@@ -70,10 +68,10 @@ pressed(Pid, Pids, K) ->
     Player = maps:get(Pid, Pids),
     Keys = Player#player.k,
     NewKeys = case K of
-        w -> Keys#keys{w = true};
-        s -> Keys#keys{s = true};
-        a -> Keys#keys{a = true};
-        d -> Keys#keys{d = true}
+        "w" -> Keys#keys{w = true};
+        "s" -> Keys#keys{s = true};
+        "a" -> Keys#keys{a = true};
+        "d" -> Keys#keys{d = true}
     end,
     NewPlayer = Player#player{k = NewKeys},
     NewPlayer.
@@ -82,38 +80,61 @@ unpressed(Pid, Pids, K) ->
     Player = maps:get(Pid, Pids),
     Keys = Player#player.k,
     NewKeys = case K of
-        w -> Keys#keys{w = false};
-        s -> Keys#keys{s = false};
-        a -> Keys#keys{a = false};
-        d -> Keys#keys{d = false}
+        "w" -> Keys#keys{w = false};
+        "s" -> Keys#keys{s = false};
+        "a" -> Keys#keys{a = false};
+        "d" -> Keys#keys{d = false}
         end,
     NewPlayer = Player#player{k = NewKeys},
     NewPlayer.
 
 collision_walls(Pids) ->
-    Player1 = maps:get(player1, Pids),
-    Player2 = maps:get(player2, Pids),
-    {X1, Y1} = Player1#player.p,
-    {X2, Y2} = Player2#player.p,
+    [{Pid1, Player1}, {Pid2, Player2}] = maps:to_list(Pids),
+
+    {FirstPlayer, FirstPid, SecondPlayer, SecondPid} = 
+        case Player1#player.first of
+            0 -> {Player1, Pid1, Player2, Pid2};
+            1 -> {Player2, Pid2, Player1, Pid1}
+        end,
+
+    {X1, Y1} = FirstPlayer#player.p,
+    {X2, Y2} = SecondPlayer#player.p,
+
     if 
         X1 =< 0 orelse X1 >= 1 orelse Y1 =< 0 orelse Y1 >= 1 ->
-            Points = Player2#player.points,
-            Player1New = Player1#player{p = (initial_pos(0))#player.p},
-            Player2New = Player2#player{
+            erlang:display("Player1 collision"),
+            Points = SecondPlayer#player.points,
+            FirstPlayerNew = FirstPlayer#player{
+                p = (initial_pos(0))#player.p,
+                a = {0,0},
+                v = {0,0},
+                k = #keys{} },
+            SecondPlayerNew = SecondPlayer#player{
                 p = (initial_pos(1))#player.p,
+                a = {0,0},
+                v = {0,0},
+                k = #keys{},
                 points = Points + 2
             },
-            UpdatedPids = Pids#{player1 => Player1New, player2 => Player2New},
+            UpdatedPids = Pids#{FirstPid => FirstPlayerNew, SecondPid => SecondPlayerNew},
             [Pid ! {pos, Player#player.p} || {Pid, Player} <- maps:to_list(UpdatedPids)],
             UpdatedPids;
         X2 =< 0 orelse X2 >= 1 orelse Y2 =< 0 orelse Y2 >= 1 ->
-            Points = Player1#player.points,
-            Player2New = Player2#player{p = (initial_pos(1))#player.p},
-            Player1New = Player1#player{
+            erlang:display("Player2 collision"),
+            Points = FirstPlayer#player.points,
+            SecondPlayerNew = SecondPlayer#player{
+                p = (initial_pos(1))#player.p,
+                a = {0,0},
+                v = {0,0},
+                k = #keys{}},
+            FirstPlayerNew = FirstPlayer#player{
                 p = (initial_pos(0))#player.p,
+                a = {0,0},
+                v = {0,0},
+                k = #keys{},
                 points = Points + 2
             },
-            UpdatedPids = Pids#{player1 => Player1New, player2 => Player2New},
+            UpdatedPids = Pids#{FirstPid => FirstPlayerNew, SecondPid => SecondPlayerNew},
             [Pid ! {pos, Player#player.p} || {Pid, Player} <- maps:to_list(UpdatedPids)],
             UpdatedPids;
         true ->
@@ -131,15 +152,15 @@ loop(Pids) ->
             NewPids2 = collision_walls(NewPids),
             [Pid ! {pos, Player#player.p} || {Pid, Player} <- maps:to_list(NewPids2)],
             loop(NewPids2);
-        % Have to change the pid to User name
         {Pid, pressed, K} ->
-            erlang:display("Hello"),
+            erlang:display("Hello1"),
             NewPlayer = pressed(Pid, Pids, K),
-            NewPids = maps:put(Pid, NewPlayer, Pids),
+            NewPids = maps:update(Pid, NewPlayer, Pids),
             loop(NewPids);
         {Pid, unpressed, K} ->
+            erlang:display("Hello2"),
             NewPlayer = unpressed(Pid, Pids, K),
-            NewPids = maps:put(Pid, NewPlayer, Pids),
+            NewPids = maps:update(Pid, NewPlayer, Pids),
             loop(NewPids);
         finished ->
             [{Pid1, _}, {Pid2, _}] = maps:to_list(Pids),
